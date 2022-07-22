f330506242004a8b2c797e87d5c0e00286a60297
Rafael Fonseca <rafael.work.34@gmail.com>
2022-07-22 17:03:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside> 
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username" >Usuario do Github</label>
+            <input name="github_username" id="github_username" req/>
+          </div>
+          
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologia</label>
+            <input name="techs" id="techs" req/>
+          </div>
+          
+          <div className="input-grup">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" req/>
+            </div>
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" req/>
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
     </div>
-  );
+
+  )
 }
 
 export default App;
