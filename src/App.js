b26640b0ef3cdab3d3988f21777a69ddc739b037
Rafael Godoi Fonseca <rafael.work.34@gmail.com>
2022-07-26 07:52:10 -0300

    estilização

--- src/App.js
+++ src/App.js
@@ -10,31 +10,37 @@ function App() {
       <aside> 
         <strong>Cadastrar</strong>
         <form>
-          <div class="input-block">
+          <div class='input-block'>
             <label htmlFor="github_username" >Usuario do Github</label>
             <input name="github_username" id="github_username" req/>
           </div>
           
-          <div class="input-block">
+          <div class='input-block'>
             <label htmlFor="techs">Tecnologia</label>
             <input name="techs" id="techs" req/>
           </div>
           
-          <div className="input-grup">
-            <div class="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" req/>
-            </div>
-            <div class="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" req/>
-            </div>
+          <div className='input-group'>
+              <div class="input-block">
+                <label htmlFor="latitude">Latitude</label>
+                <input name="latitude" id="latitude" req/>
+              </div>
+              <div class="input-block">
+                <label htmlFor="longitude">Longitude</label>
+                <input name="longitude" id="longitude" req/>
+              </div>
           </div>
           <button type="submit">Salvar</button>
         </form>
       </aside>
       <main>
-
+        <ul>
+          <li className="dev-item">
+            <header>
+              
+            </header>
+          </li>   
+        </ul>
       </main>
     </div>
 
