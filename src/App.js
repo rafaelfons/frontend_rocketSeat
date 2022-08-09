32a764689ad1d170f8c0d6010710673b8ab2fb76
Rafael Fonseca <rafael.work.34@gmail.com>
2022-08-09 17:20:04 -0300

    Model

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import api from './services/api'
-
+import Modal from './Modal'
 import './global.css'
 import './App.css'
 import './Sidebar.css'
@@ -10,17 +10,18 @@ import './Main.css'
 
 
 function App() {
+
   const [devs, setDevs] = useState([])
   const [latitude, setLatitude] = useState('')
   const [longitude, setLongitude] = useState('')
-  const [github_username, setGithub_username ] = useState('')
+  const [github_username, setGithub_username] = useState('')
   const [techs, setTechs] = useState('')
-  
-    
+  const [show, setShow] = useState(false);
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        const {longitude, latitude} = position.coords
+        const { longitude, latitude } = position.coords
 
         setLatitude(latitude)
         setLongitude(longitude)
@@ -34,89 +35,97 @@ function App() {
     )
   }, [])
 
-  useEffect((devs) => {
-    async function loadDevs() {
-      const response = await api.get('/devs')
+  async function loadDevs() {
+    const response = await api.get('/devs')
+    setDevs(response.data)
+  }
 
-      
-
-      setDevs(response.data)        
-      }
-      loadDevs()
-  }, [])
+  useEffect(() => {
+   
+    loadDevs()
+  }, []);
 
   async function handleAddDev(e) {
     e.preventDefault()
-
+    console.log(devs, "DEVS")
     const response = await api.post('/create', {
       github_username,
       techs,
       latitude,
       longitude,
-    } )
+    })
     setGithub_username('')
     setTechs('')
 
-    
-    setDevs(...devs, response.data)
-  
+    setDevs([...devs, response.data.dev])
+
   }
- 
+
+  function index(e, dev) {
+    e.preventDefault();
+        
+    setGithub_username(dev.github_username)
+    setTechs(dev.techs)
+    setLongitude (dev.location.coordinates[0])
+    setLatitude(dev.location.coordinates[1])
+    setShow(!show)
+  }
+
   return (
     <div id="app">
-      <aside> 
+      <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev} >
+        <form onSubmit={handleAddDev}>
           <div className='input-block'>
             <label htmlFor="github_username" >Usuário do Github</label>
-            <input name="github_username" 
-            id="github_username" 
-            required
-            value={github_username}
-            onChange={e => setGithub_username(e.target.value)} 
+            <input name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithub_username(e.target.value)}
             />
           </div>
-          
+
           <div className='input-block'>
             <label htmlFor="techs">Tecnologia</label>
-            <input name="techs" 
-            id="techs" 
-            required
-            value={techs}
-            onChange={e => setTechs(e.target.value)} 
+            <input name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
             />
           </div>
-          
+
           <div className='input-group'>
-              <div className="input-block">
-                <label htmlFor="latitude">Latitude</label>
-                <input type="number" 
-                name="latitude" 
-                id="latitude" 
-                required 
-                value={latitude} 
-                onChange={e => setLatitude(e.target.value)} 
-                />
-              </div>
-              <div className="input-block">
-                <label htmlFor="longitude">Longitude</label>
-                <input type = "number" 
-                name="longitude" 
-                id="longitude" 
-                required 
-                value={longitude} 
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
                 onChange={e => setLongitude(e.target.value)}
-                />
-              </div>
+              />
+            </div>
           </div>
           <button type="submit">Salvar</button>
         </form>
       </aside>
-     
+
       <main>
         <ul>
-            {devs.map ( dev =>(
-              <li key={dev._id} className="dev-item">
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
               <header>
                 <img src={dev.avatar_url} alt={dev.name} />
                 <div className="user-info">
@@ -126,12 +135,15 @@ function App() {
               </header>
               <p>{dev.bio}</p>
               <a href={`https://github.com/${dev.github_username}`}>Acessar perfil GitHub</a>
-            </li>  
-          ))}         
-          </ul>
-        </main>
-      </div>
-
+              <button onClick={e => index(e, dev)}>Atualizar</button>
+            </li>
+          ))}
+        </ul>
+      </main>
+      {
+        show ? <Modal github_username={github_username} techs={techs} latitude={latitude} longitude={longitude} loadDevs={loadDevs}></Modal> : <div />
+      }
+    </div>
   )
 }
 
---
+++ src/Modal.js
@@ -0,0 +1,81 @@
+import React, {useState} from 'react'
+import api from './services/api'
+
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
+
+function Modal(props) {
+    const [latitude, setLatitude] = useState(props.latitude)
+    const [longitude, setLongitude] = useState(props.longitude)
+    const [github_username, setGithub_username ] = useState(props.github_username)
+    const [techs, setTechs] = useState(props.techs)
+    console.log(props)
+
+    async function handleUpdate(e) {
+        console.log("OK")
+        e.preventDefault()
+        const response = await api.patch('/update', {
+          github_username,
+          techs,
+          latitude,
+          longitude,
+        } )
+        console.log(response)    
+        props.loadDevs();    
+      }
+    
+    return (
+        <aside className="Modal">
+            <strong>Atualizar</strong>
+            <form onSubmit={handleUpdate}>
+                <div className='input-block'>
+                    <label htmlFor="github_username" >Usuário do Github</label>
+                    <input name="github_username"
+                        id="github_username"
+                        required
+                        value={github_username}
+                        onChange={e => setGithub_username(e.target.value)}
+                    />
+                </div>
+
+                <div className='input-block'>
+                    <label htmlFor="techs">Tecnologia</label>
+                    <input name="techs"
+                        id="techs"
+                        required
+                        value={techs}
+                        onChange={e => setTechs(e.target.value)}
+                    />
+                </div>
+
+                <div className='input-group'>
+                    <div className="input-block">
+                        <label htmlFor="latitude">Latitude</label>
+                        <input type="number"
+                            name="latitude"
+                            id="latitude"
+                            required
+                            value={latitude}
+                            onChange={e => setLatitude(e.target.value)}
+                        />
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="longitude">Longitude</label>
+                        <input type="number"
+                            name="longitude"
+                            id="longitude"
+                            required
+                            value={longitude}
+                            onChange={e => setLongitude(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <button type="submit">Atualiza</button>
+            </form>
+        </aside>
+    )
+}
+
+export default Modal;
